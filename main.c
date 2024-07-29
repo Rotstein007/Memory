968500fb30af196fbb0ab2614620eb815bbd3b37
Rotstein007 <felix04.hofer@gmail.com>
2024-07-29 16:53:55 +0200

    commit

--- main.c
+++ main.c
@@ -63,15 +63,12 @@ int main(int argc, char **argv) {
     // Initialisieren Sie den Zufallszahlengenerator
     srand(time(NULL));
 
-    GtkApplication *app;
-    int status;
-
     // Erstellen Sie eine neue GTK-Anwendung
-    app = gtk_application_new("com.example.GtkApplication", G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication * app = gtk_application_new("com.example.GtkApplication", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
     // Starten Sie die GTK-Anwendung
-    status = g_application_run(G_APPLICATION(app), argc, argv);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
 
     return status;
